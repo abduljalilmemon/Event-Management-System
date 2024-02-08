5942c786a4289a123fd82fe5878f0c67bbfc228c
Abdul Jalil <abduljaleelmemon3@gmail.com>
2024-02-08 23:21:35 +0500

    update image for all pages and detail changes in home page

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('/', views.home, name='home'),
     path('login', views.login, name='login'),
     path('event', views.event, name='event'),
     path('detail', views.get_detail, name='detail'),
