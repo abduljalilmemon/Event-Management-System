4e6160331b51d68b436dbee9f68e902578c0b717
Abdul Jalil <abduljaleelmemon3@gmail.com>
2024-02-10 23:12:01 +0500

    added form for adding participant

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,13 @@
 # forms.py
 from django import forms
+from .models import Participant
 
 
 class ImportForm(forms.Form):
     csv_file = forms.FileField(label='Select a CSV file')
+
+
+class AddParticipantForm(forms.ModelForm):
+    class Meta:
+        model = Participant
+        fields = '__all__'
